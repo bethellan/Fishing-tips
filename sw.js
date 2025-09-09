1969be0f898089291dbe7818b0c17175d1364220
bethellan <bethellan@hotmail.com>
2025-09-10 00:31:44 +1200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+// Simple service worker v5
+const CACHE='fishing-planner-v5';
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest']))) });
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))))});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))) });
