96c728a3b1ed9a37e056a8c311174e29e88524a4
bethellan <bethellan@hotmail.com>
2025-09-11 18:53:52 +1200

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-// Simple service worker v15
-const CACHE='fishing-v15';
-self.addEventListener('install',e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./spots.json'])));
-});
-self.addEventListener('fetch',e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
-});
